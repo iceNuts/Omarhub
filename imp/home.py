48e5500d1462dcbcb377f55fef27f31010d12b84
zlychaos <zlychaos@gmail.com>
2012-06-24 23:31:09 +0800

    Add mistakes_to_be.txt to note the potential mistakes; Add profile.py as user profile page's handler; Modify home.py to do some minor changes, nothing special;

--- imp/home.py
+++ imp/home.py
@@ -3,5 +3,14 @@ import tornado.auth
 from base import *
 
 class HomeHandler(BaseHandler):
+	def initialize(self):
+		self.dbManager = dbmgr()
 	def get(self):
+		if not self.current_user:
+			self.redirect("/auth/login")
+		mail=self.current_user
+		
 		self.render("home.html")
+		
+	def post(self):
+		pass
---
+++ imp/profile.py
@@ -0,0 +1,16 @@
+import tornado.web
+import tornado.auth
+from base import *
+
+class ProfileHandler(BaseHandler):
+	def initialize(self):
+		self.dbManager = dbmgr()
+		
+	def get(self):
+		if not self.current_user:
+			self.redirect("/auth/login")
+		mail=self.current_user
+		user=self.dbManager.get_user_profile_info(mail)
+		followers=self.dbManager.get_follower(mail)
+		followering=self.dbManager.get_following(mail)
+		self.render("profile.html",user=user,followers=followers,following=following)
