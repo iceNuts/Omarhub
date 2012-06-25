d4e98695a73af54e026ec87f867c9f5b3834b410
Bill Zeng <billzeng808@gmail.com>
2012-06-25 15:06:32 +0800

    Add more APIs in db.py to support profile page

--- imp/main.py
+++ imp/main.py
@@ -10,6 +10,7 @@ from home import *
 from activate import *
 from loginUsername import *
 from content import *
+from profile import *
 
 class MainHandler(BaseHandler):
 	def get(self):
@@ -33,7 +34,9 @@ class Application(tornado.web.Application):
             (r"/content/provider/getevents", EventProvider),
             (r"/content/provider/getoffers", OfferProvider),
             (r"/content/provider/getneeds", NeedProvider),
-            (r"/content/provider/getrecentall", RecentallProvider)
+            (r"/content/provider/getrecentall", RecentallProvider),
+			(r"/profile",ProfileHandler),
+			(r"/profile/editMyProfile", ProfileEditHandler)
 			]
 		settings = dict(
 			template_path=os.path.join(os.path.dirname(__file__), "template"),
--- imp/profile.py
+++ imp/profile.py
@@ -1,5 +1,6 @@
 import tornado.web
 import tornado.auth
+from core.db import *
 from base import *
 
 class ProfileHandler(BaseHandler):
@@ -14,3 +15,36 @@ class ProfileHandler(BaseHandler):
 		followers=self.dbManager.get_follower(mail)
 		followering=self.dbManager.get_following(mail)
 		self.render("profile.html",user=user,followers=followers,following=following)
+
+
+class ProfileEditHandler(BaseHandler):
+	def initialize(self):
+		self.dbManager = dbmgr()
+	
+	def get(self):
+		if not self.current_user:
+			self.redirect("/auth/login")
+		pass
+			
+	def post(self):
+		if not self.current_user:
+			return None
+		mail = self.current_user
+#fetch input data
+#user can't change the mail
+		first_name = self.get_arguments("first_name")
+		last_name = self.get_arguments("last_name")
+		age = self.get_arguments("age")
+		location = self.get_arguments("location")
+		target_population = self.get_arguments("target_population")
+		work_field = self.get_arguments("work_field")
+		language = self.get_arguments("language")
+		country = self.get_arguments("country")
+		skype = self.get_arguments("skype")
+		
+		
+		
+
+
+
+
